ff7d11e95a62d1bbee8611edadd0de02353fc141
jongrover <hello@jonathangrover.com>
2015-02-04 14:18:51 -0500

    remove solution

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var fruits = new Fruits([
-  { img: 'apple.png', title: 'Apple', qty: 3, price: 0.49 },
-  { img: 'orange.png', title: 'Orange', qty: 7, price: 0.52 },
-  { img: 'pear.png', title: 'Pear', qty: 1, price: 0.89 },
-  { title: 'Star Fruit', qty: 1, price: '1.25' }
-]);
-var router = new Router();
-Backbone.history.start();
-router.navigate('fruits', {trigger: true });
--- js/views/fruits-index-view.js
+++
@@ -1,9 +0,0 @@
-FruitsIndexView = Backbone.View.extend({
-
-  template: _.template($('#fruits-index-template').html()),
-
-  render: function () {
-    this.$el.html(this.template({ fruits: this.collection.toJSON() }));
-    return this.el;
-  }
-});
